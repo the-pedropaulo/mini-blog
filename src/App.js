7916145c8f85ab8a088c27dbe08ae9dd2d339ea7
Pedro Paulo <pedro98seabra@gmail.com>
2021-07-13 15:52:43 -0300

    Subindo arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import axios from 'axios'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  const [posts, setNewPosts] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    
+    axios.get('https://alemdocodigo.herokuapp.com/list_posts')
+	.then((response) => {
+		setNewPosts(response.data.posts)
+		setLoading(false)
+    }).catch(() => {
+		console.log('deu errado')
+	})
+    
+	}, [])
+
+
+	return(
+		<div className="app">
+
+			<div className="cards">
+
+				{ loading ? 
+				
+						( <div class="loadingio-spinner-rolling-ej17kcce5w5"><div class="ldio-wix3oy7gesp">
+						<div></div>
+						</div></div> ) :
+						
+						posts.map((post, key) => {
+							return(
+							<div className="card" key={key} >
+								<div className="card-body" >
+									<h1>{post.title}</h1>
+									<div className="line"></div>
+									<h2>{post.content}</h2>
+								</div>
+						</div>
+						)
+					})
+				}
+
+			</div>
+
+		</div>
+	)
+
 }
 
 export default App;
